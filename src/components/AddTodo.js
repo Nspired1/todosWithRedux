926b55c084f42a9af15c88ba24ecef1a53b4e992
Nspired1 <don.spire1@gmail.com>
2020-11-20 10:02:16 -0800

    minor update

---
+++ src/components/AddTodo.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { connect } from "react-redux";
+import { addTodo } from "../actions";
+
+let AddTodo = ({ dispatch }) => {
+  let input;
+  return (
+    <div>
+      <input
+        ref={(node) => {
+          input = node;
+        }}
+      />
+      <button
+        onClick={() => {
+          dispatch(addTodo(input.value));
+          input.value = "";
+        }}
+      >
+        Add Todo
+      </button>
+    </div>
+  );
+};
+
+AddTodo = connect(null, null)(AddTodo);
+
+export default AddTodo;
